ec005dcbac3496a3040972625c2ba7b97210175e
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-07 10:37:41 +0500

    backend2 PostgreSQL connection

--- backend2/src/app.module.ts
+++ backend2/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      username: "postgres",
+      host: "localhost",
+      port: 5432,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
