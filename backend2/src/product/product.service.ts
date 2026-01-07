7e48eeb94a5b41d2c87d4f2b78def1649ae3026d
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-07 17:51:59 +0500

    added product backend2

---
+++ backend2/src/product/product.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Product } from "./entities/product.entity";
+import { Repository } from "typeorm";
+
+@Injectable()
+export class ProductService {
+  constructor(
+    @InjectRepository(Product) private productRepo: Repository<Product>
+  ) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const product = this.productRepo.create(createProductDto);
+    return await this.productRepo.save(product);
+  }
+
+  async findAll() {
+    return await this.productRepo.find();
+  }
+
+  async findOne(id: number) {
+    const product = await this.productRepo.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException("Product not found");
+    }
+    return product;
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.productRepo.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException("Product not found");
+    }
+    return await this.productRepo.update(id, updateProductDto);
+  }
+
+  async remove(id: number) {
+    const product = await this.productRepo.findOneBy({ id });
+    if (!product) {
+      throw new NotFoundException("Product not found");
+    }
+    return await this.productRepo.delete(id);
+  }
+}
