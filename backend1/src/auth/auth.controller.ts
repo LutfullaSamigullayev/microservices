7d9c28efb92ec143293cf898731cce96be72f602
LutfullaSamigullayev <qwerty.sayt@gmail.com>
2026-01-08 09:25:59 +0500

    added event pattern

--- backend1/src/auth/auth.controller.ts
+++ backend1/src/auth/auth.controller.ts
@@ -2,10 +2,16 @@ import { Controller, Post, Body } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { RegisterAuthDto } from "./dto/register-auth.dto";
 import { LoginAuthDto } from "./dto/login-auth.dto";
+import { EventPattern } from "@nestjs/microservices";
 
 @Controller("auth")
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @EventPattern('hello')
+  async hello(data: string) {
+    console.log(data);
+  }
 
   @Post()
   register(@Body() registerAuthDto: RegisterAuthDto) {
